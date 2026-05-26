f823ddad7408224fac87e3badcaa31bbe8d198ef
PietroWilhelm <pietrowilhelm@git.com>
2026-05-26 13:59:07 -0300

    fix: arrumando Atribução e nome

--- CarbonTrace.Domain/Commom/BaseEntity.cs
+++ CarbonTrace.Domain/Commom/BaseEntity.cs
@@ -4,7 +4,7 @@ public abstract class BaseEntity
 {
     public Guid Id { get; set; }
     
-    public bool active { get; set; } 
+    public bool Active { get; set; } 
     
     public DateTime CreatedAt{ get; set; } = DateTime.UtcNow;
 }
--- CarbonTrace.Domain/Entities/Estado.cs
+++ CarbonTrace.Domain/Entities/Estado.cs
@@ -4,8 +4,8 @@ namespace CarbonTrace.Domain.Entities;
 
 public class Estado(string nome, string sigla)  : BaseEntity
 {
-    private string Nome { get; set; }
-    private string Sigla { get; set; }
+    public string Nome { get; set; }
+    public string Sigla { get; set; }
     
     
     // 1:N
