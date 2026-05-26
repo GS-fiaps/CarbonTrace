988de1b46527645b3de3421aade2e1fe09e85041
PietroWilhelm <pietrowilhelm@git.com>
2026-05-26 11:56:11 -0300

    chore: organização do projeto

--- CarbonTrace.API/obj/Debug/net10.0/CarbonTrace.AssemblyInfo.cs
+++ CarbonTrace.API/obj/Debug/net10.0/CarbonTrace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarbonTrace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b144002ac0291e3627ae90b56c2f66422f67a9cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarbonTrace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarbonTrace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
